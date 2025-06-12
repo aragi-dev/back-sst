e1570af93c62d62a8d95ffae11b4c685cdf10345
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-12 17:52:37 -0600

    Update getProduct.ts

--- api/getProduct.ts
+++ api/getProduct.ts
@@ -3,14 +3,19 @@ import type { APIGatewayProxyHandler } from "aws-lambda";
 import { container } from "tsyringe";
 import { ProductService } from "@services/ProductService";
 import "@utils/injector";
-
+import response from "@utils/adapters/responseHandler";
+import { messages } from "@utils/messages";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-	const service = container.resolve(ProductService);
-	const result = await service.getAll();
-	return {
-		statusCode: 200,
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ data: result }),
-	};
+  try {
+    const service = container.resolve(ProductService);
+    const result = await service.getAll();
+    return response(result);
+  } catch (error) {
+    return response({
+      statusCode: messages.statusCode.INTERNAL_SERVER_ERROR,
+      message: messages.error.SERVICE,
+      data: error,
+    });
+  }
 };
