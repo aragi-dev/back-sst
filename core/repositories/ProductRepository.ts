f4369ae453058a8be0b05f1ac52cb07a029b111f
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-12 17:56:40 -0600

    Update ProductRepository.ts

--- core/repositories/ProductRepository.ts
+++ core/repositories/ProductRepository.ts
@@ -1,42 +1,72 @@
 import { connectDB } from "@utils/typeorm";
 import { Product } from "@core/entities/Product";
 import type { IProductRepository } from "@core/IRepositories/IProductRepository";
+import type { EntityManager } from "typeorm";
+import { LogRepository } from "@utils/loggers/repositoryDecorator";
 
-export class ProductRepository implements IProductRepository {
-  async findAll(): Promise<Product[]> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
+export class ProductRepository implements ProductRepository {
+  @LogRepository
+  async findAll(manager?: EntityManager): Promise<Product[]> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
     return repo.find();
   }
 
-  async create(product: Partial<Product>): Promise<Product> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
-    return repo.save(product);
+  @LogRepository
+  async create(
+    data: Partial<Product>,
+    manager?: EntityManager
+  ): Promise<Product> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
+    return repo.save(data);
   }
 
-  async findByParams(params: Partial<Product>): Promise<Product | null> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
-    return repo.findOneBy(params);
+  @LogRepository
+  async findByParams(
+    data: Partial<Product>,
+    manager?: EntityManager
+  ): Promise<Product | null> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
+    return repo.findOneBy(data);
   }
 
-  async findById(id: string): Promise<Product | null> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
+  @LogRepository
+  async findById(id: string, manager?: EntityManager): Promise<Product | null> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
     return repo.findOneBy({ id });
   }
 
-  async update(id: string, data: Partial<Product>): Promise<Product | null> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
+  @LogRepository
+  async update(
+    id: string,
+    data: Partial<Product>,
+    manager?: EntityManager
+  ): Promise<Product | null> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
     await repo.update(id, data);
     return repo.findOneBy({ id });
   }
 
-  async delete(id: string): Promise<boolean> {
-    const db = await connectDB();
-    const repo = db.getRepository(Product);
+  @LogRepository
+  async delete(id: string, manager?: EntityManager): Promise<boolean> {
+    const db = manager || (await connectDB());
+    const repo = manager
+      ? manager.getRepository(Product)
+      : db.getRepository(Product);
     const result = await repo.delete(id);
     return result.affected !== 0;
   }
