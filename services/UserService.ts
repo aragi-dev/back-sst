56890783911a5d69199818dc554088e796de686d
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-13 17:41:19 -0600

    Update UserService.ts

--- services/UserService.ts
+++ services/UserService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import type {
+  ICreateResponse,
   ICreateUserDto,
   ILoginResponse,
   ILoginUserDto,
@@ -14,7 +15,9 @@ import { ExpireTimeEnum } from "@utils/enums/ExpireTimeEnum";
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 import { AppDataSource } from "@utils/typeorm";
 import { LogUseCase } from "@utils/loggers/useCaseDecorator";
-
+import { authenticator } from "otplib";
+import qrcode from "qrcode";
+import { sendEmail } from "@services/EmailService";
 @injectable()
 export class UserService {
   constructor(
@@ -23,34 +26,35 @@ export class UserService {
   ) {}
 
   @LogUseCase
-  async create(data: ICreateUserDto): Promise<UseCase<User>> {
+  async create(data: ICreateUserDto): Promise<UseCase<ICreateResponse>> {
     const dataBase = await AppDataSource.createQueryRunner();
+
     try {
       await dataBase.connect();
       await dataBase.startTransaction();
 
       const existingUser = await this.userRepository.findByParams(
-        {
-          email: data.email,
-          name: data.name,
-        },
+        { email: data.email },
         dataBase.manager
       );
+
       if (existingUser) {
         await dataBase.rollbackTransaction();
         return {
           statusCode: messages.statusCode.CONFLICT,
           message: messages.error.ALREADY_EXISTS,
-          data: undefined as unknown as User,
+          data: undefined as unknown as ICreateResponse,
         };
       }
 
-      const passwordHash = await bcrypt.hash(data.password, 10);
-      const newUser = await this.userRepository.create(
+      const mfaSecret = authenticator.generateSecret();
+      const otpauth = authenticator.keyuri(data.email, "comberter", mfaSecret);
+      const qrCodeDataUrl = await qrcode.toDataURL(otpauth);
+
+      await this.userRepository.create(
         {
-          name: data.name,
           email: data.email,
-          password_hash: passwordHash,
+          mfaSecret,
           role: data.role,
           status: true,
         },
@@ -58,17 +62,29 @@ export class UserService {
       );
 
       await dataBase.commitTransaction();
+
+      await sendEmail({
+        to: data.email,
+        subject: "Configura tu MFA",
+        htmlBody: `
+        <h1>Tu MFA está listo</h1>
+        <p>Escanea este código QR:</p>
+        <img src="${qrCodeDataUrl}" />
+        <p>O ingresa este código manual: <strong>${otpauth}</strong></p>
+        `,
+      });
+
       return {
         statusCode: messages.statusCode.SUCCESS,
         message: messages.success.CREATED,
-        data: newUser,
+        data: { email: data.email },
       };
     } catch (error) {
       await dataBase.rollbackTransaction();
       return {
         statusCode: messages.statusCode.INTERNAL_SERVER_ERROR,
         message: messages.error.SERVICE,
-        data: undefined as unknown as User,
+        data: undefined as unknown as ICreateResponse,
       };
     } finally {
       await dataBase.release();
@@ -77,19 +93,21 @@ export class UserService {
 
   @LogUseCase
   async login(data: ILoginUserDto): Promise<UseCase<ILoginResponse>> {
+    const email = "test@gmail.com";
     const user = await this.userRepository.findByParams({
-      email: data.email,
+      email: email,
     });
-    if (!user || !user.status) {
+    if (!user || !user.status || user.mfaSecret) {
       return {
         statusCode: messages.statusCode.UNAUTHORIZED,
         message: messages.error.NOT_AVAILABLE,
         data: undefined as unknown as ILoginResponse,
       };
     }
-    const match = await bcrypt.compare(data.password, user.password_hash);
 
-    if (!match) {
+    const isValid = authenticator.check(data.code, user.mfaSecret);
+
+    if (!isValid) {
       return {
         statusCode: messages.statusCode.UNAUTHORIZED,
         message: messages.error.INVALID_PARAMETERS,
@@ -106,7 +124,6 @@ export class UserService {
         token,
         user: {
           id: user.id,
-          name: user.name,
           role: user.role,
         },
       },
