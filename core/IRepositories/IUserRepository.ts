d520932aa5079aa44f7fdb86145ec884c13776e5
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-13 16:19:16 -0600

    Update IUserRepository.ts

--- core/IRepositories/IUserRepository.ts
+++ core/IRepositories/IUserRepository.ts
@@ -1,10 +1,11 @@
 import type { User } from "@core/entities/User";
+import type { EntityManager } from "typeorm";
 
 export interface IUserRepository {
-  findAll(): Promise<User[]>;
-  create(user: Partial<User>): Promise<User>;
-  findByParams(params: Partial<User>): Promise<User | null>;
-  findById(id: string): Promise<User | null>;
-  update(id: string, data: Partial<User>): Promise<User | null>;
-  delete(id: string): Promise<boolean>;
+  findAll(manager?: EntityManager): Promise<User[]>;
+  create(user: Partial<User>, manager?: EntityManager): Promise<User>;
+  findByParams(params: Partial<User>, manager?: EntityManager): Promise<User | null>;
+  findById(id: string, manager?: EntityManager): Promise<User | null>;
+  update(id: string, data: Partial<User>, manager?: EntityManager): Promise<User | null>;
+  delete(id: string, manager?: EntityManager): Promise<boolean>;
 }
